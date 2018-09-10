5be0011583ba569926e65ccfcfea51d4d2d87c27
mro5772 <mro5772@IGM2025-18>
2018-09-10 19:31:42 -0400

    adding the goods

---
+++ Assets/Scripts/PlayerControls.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+   public Vector3 mousePosition;
+    public float speed = 0.1f;
+    
+
+    public Vector3 velocity;
+    //acceleration
+    public Vector3 acceleration;
+
+    public Vector3 vehiclePosition;
+
+    // Use this for initialization
+    void Start () {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButton(0))
+        {
+            UpdatePosition();
+            getMouseLocation();
+        }
+    }
+    void UpdatePosition()
+    {
+        // Grab the transform's position so the character
+        //   is updated every frame
+
+        vehiclePosition = transform.position;
+
+        // Add accel to velocity
+        velocity += acceleration;
+
+        velocity = Vector3.ClampMagnitude(velocity, speed);
+
+        // Add velocity to position
+        vehiclePosition += velocity;
+
+        // Start "fresh" with accel
+        acceleration = Vector3.zero;
+
+        vehiclePosition.z = 0;
+        // Set the transform
+        transform.position = vehiclePosition;
+
+
+    }
+
+    public void getMouseLocation()
+    {
+       
+           mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            Vector3 desiredPos= mousePosition - vehiclePosition;
+            desiredPos.Normalize();
+            desiredPos *= speed;
+
+            acceleration += desiredPos;
+    }
+}
