9717931fbe1f34e40eeb26ccc62af0dc5c0a06e5
mro5772 <mro5772@IGM2025-18>
2018-09-10 21:41:43 -0400

    adding swipestuff

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class PlayerControls : MonoBehaviour {
    public Vector3 mousePosition;
     public float speed = 0.1f;
-    
+    public float swipeSpeed = 1f;
+    public CircleCollider2D colider;
+
 
     public Vector3 velocity;
     //acceleration
@@ -13,18 +15,51 @@ public class PlayerControls : MonoBehaviour {
 
     public Vector3 vehiclePosition;
 
+    public Swipe swipeControls;
+    public Transform player;
+    private Vector3 desiredPosition;
+
+    public float swipeTimer;
+   public float swipeRecharge;
+    public float rechargeFloat;
+
+
     // Use this for initialization
     void Start () {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        swipeTimer = 0;
+        swipeRecharge = 0;
     }
 	
 	// Update is called once per frame
 	void Update () {
+        swipeRecharge++;
+        if (swipeRecharge >= rechargeFloat)
+        {
+            swipeRecharge = rechargeFloat;
+            acceleration = Vector3.zero;
+            desiredPosition = Vector3.zero;
+            velocity = Vector3.zero;
+        }
+
+        swipeTimer--;
+        if (swipeTimer <= 0)
+        {
+            swipeTimer = 0;
+        }
+
+
+        UpdatePosition();
         if (Input.GetMouseButton(0))
         {
-            UpdatePosition();
-            getMouseLocation();
+           
+            //  getMouseLocation();
+            swipePosition();
         }
+
+       
+
+      
     }
     void UpdatePosition()
     {
@@ -48,7 +83,7 @@ public class PlayerControls : MonoBehaviour {
         // Set the transform
         transform.position = vehiclePosition;
 
-
+        // player.transform.position = Vector3.MoveTowards(player.transform.position, desiredPosition, 3f * Time.deltaTime);
     }
 
     public void getMouseLocation()
@@ -61,5 +96,43 @@ public class PlayerControls : MonoBehaviour {
             desiredPos *= speed;
 
             acceleration += desiredPos;
+    }
+
+    public void swipePosition()
+    {
+       swipeRecharge = 0;
+            swipeTimer = 5;
+            if (swipeControls.SwipeLeft)
+            {
+                desiredPosition += Vector3.left;
+                Debug.Log("left");
+            }
+
+            if (swipeControls.SwipeRight)
+            {
+                desiredPosition += Vector3.right;
+                Debug.Log("right");
+            }
+
+            if (swipeControls.SwipeUp)
+            {
+                desiredPosition += Vector3.up;
+                Debug.Log("up");
+            }
+
+            if (swipeControls.SwipeDown)
+            {
+                desiredPosition += Vector3.down;
+                Debug.Log("down");
+            }
+            desiredPosition.Normalize();
+            desiredPosition *= swipeSpeed;
+            acceleration += desiredPosition;
+
+
+
+
+
+
     }
 }
---
+++ Assets/Scripts/Swipe.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swipe : MonoBehaviour {
+
+    private bool tap, swipeLeft, swipeRight, swipeUp, swipeDown;
+   public Vector2 startTouch, swipeDelta;
+    private bool isDragin = false;
+
+
+    private void Update()
+    {
+        tap = swipeLeft = swipeDown = swipeRight = swipeUp = false;
+
+        #region Standalone Inputs
+        if (Input.GetMouseButtonDown(0))
+        {
+            isDragin = true;
+            tap = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Reset();
+        }
+
+        #endregion
+
+
+        #region mobile inputs
+        if(Input.touches.Length > 0)
+        {
+            if(Input.touches[0].phase == TouchPhase.Began)
+            {
+                isDragin = true;
+                tap = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if(Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragin = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        //calculate distance
+        swipeDelta = Vector2.zero;
+
+       
+        if (isDragin)
+        {
+            if (Input.touches.Length > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+           else if (Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }
+        }
+
+    
+
+        //did we cross the deadzone?
+        if(swipeDelta.magnitude > 200)
+        {
+            //which direction?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+            if(Mathf.Abs(x) > 0)
+            {
+                //left or right
+                if (x < 0)
+                    swipeLeft = true;
+                if (x > 0)
+                    swipeRight = true;
+            }
+            if (Mathf.Abs(y) > 0)
+            {
+                //up or down
+                if (y < 0)
+                    swipeDown = true;
+                if (y > 0)
+                    swipeUp = true;
+            }
+
+            Reset();
+        }
+     
+    }
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDragin = false;
+    }
+
+
+    public bool Tap { get { return Tap; } }
+    public Vector2 SwipeDelta { get { return swipeDelta; } }
+    public bool SwipeLeft { get { return swipeLeft; } }
+    public bool SwipeRight{ get { return swipeRight; } }
+    public bool SwipeDown { get { return swipeDown; } }
+    public bool SwipeUp { get { return swipeUp; } }
+
+
+}
